967bed031cec9a4aa35ad2a97bacf24faaec5cf4
Mr.LEE <sm895959@gmail.com>
2024-12-02 11:51:09 +0900

    클래스 정의를 활용하여 게임 만들어보기

---
+++ main.py
@@ -0,0 +1,28 @@
+import random
+from character import Hero, Goblin, Orc, Drangon
+from battle import Battle
+
+
+def main():
+    print("게임 시작")
+    name = input("이름 입력:")
+    role = input("직업 입력(전사/마법사/궁수)")
+    hero = Hero(name, 100, 20, 5, speed=12, role=role)
+    print(hero)
+
+    monster_pool = [Goblin("고블린", 50, 10, 2, 1, 1),
+                    Orc("오크", 50, 10, 2, 1, 1),
+                    Drangon("드래곤", 50, 10, 2, 1, 1)]
+
+    battle = Battle()
+    while hero.is_alive():
+        monster = random.choice(monster_pool)
+        print(f"\n 몬스터 {monster.name} 등장!")
+        print(monster.description())
+
+        if not battle.fight(hero, monster):
+            break
+
+
+if __name__ == '__main__':
+    main()
